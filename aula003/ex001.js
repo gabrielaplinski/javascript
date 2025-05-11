3bffcfc06d0061bb2cb43a75448edcb8552f2b1b
Gabriela Piovesan Plinski <gabriela.dev@outlook.com>
2025-05-10 23:10:09 -0300

    exs javascript

---
+++ aula003/ex001.js
@@ -0,0 +1,1 @@
+console.log('Meu nome é "Gabriela". Estou aprendendo JavaScript ás', 11, 'h da noite');
---
+++ aula003/index.js
@@ -0,0 +1,4 @@
+console.log('Exemplo "literal"'); //string
+console.log(`'Exemplo' "literal"`);
+
+console.log(35, 15.85, 'Exemplo numérico'); //number 
