e9ee7f44e51162661ed3028b62cc3c296bc375ba
Gabriela Piovesan Plinski <gabriela.dev@outlook.com>
2025-05-16 22:45:00 -0300

    aula 6 js

---
+++ aula006/index.js
@@ -0,0 +1,7 @@
+let nome = 'Aurora'; //string
+
+console.log(nome, 'nasceu em 2022')
+console.log('Com 4 anos', nome, 'irá para a escola')
+console.log(nome, 'é muito engraçada e criativa')
+console.log('Gabriela e Rafael amam demais a', nome)
+console.log('É incrível viver com a', nome, '!')
---
+++ aula006/index001.js
@@ -0,0 +1,5 @@
+let curso; //declarou variável
+curso = 'Sistemas para Internet'; // Inicializando a variável
+console.log('Estou cursando', curso);
+curso = 'Análise e Desenvolvimento de Sistemas';
+console.log('Estou cursando', curso);
