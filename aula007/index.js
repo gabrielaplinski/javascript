d9b829a39dc5b72c53736fbcf754811caa2dbdcf
Gabriela Piovesan Plinski <gabriela.dev@outlook.com>
2025-05-20 22:53:05 -0300

    ex js

---
+++ aula007/index.js
@@ -0,0 +1,9 @@
+const primeiroNum = 5;
+const segundoNum = 10;
+const conta = primeiroNum * segundoNum;
+const contaDuplicada = conta * 2;
+let contaTriplicada = conta * 3;
+contaTriplicada = contaTriplicada + 5;
+
+console.log(contaTriplicada);
+console.log(typeof primeiroNum);
