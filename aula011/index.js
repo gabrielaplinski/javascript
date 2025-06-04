eafe30818e129b901d7a0380d159faae3ad329f4
Gabriela Piovesan Plinski <gabriela.dev@outlook.com>
2025-06-03 23:08:41 -0300

    Create index.js

---
+++ aula011/index.js
@@ -0,0 +1,14 @@
+let contador = 1;
+contador++;
+
+console.log(contador);
+
+let exemplo = 3;
+exemplo *= 5;
+
+console.log(exemplo);
+
+const ex1 = 10;
+const ex2 = Number('5'); // Tranforma a string em number
+
+console.log(ex1 + ex2);
