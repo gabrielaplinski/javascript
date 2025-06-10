3e099701be5b59b9e3ec07049c4bcd7aaa0d8001
Gabriela Piovesan Plinski <gabriela.dev@outlook.com>
2025-06-09 22:17:12 -0300

    EX window js

--- aula012/js/script.js
+++ aula012/js/script.js
@@ -2,9 +2,14 @@ let num = prompt('Digite um número');
 
 let num2 = prompt('Digite outro número');
 
-num = parseFloat(num);
-num2 = parseFloat(num2);
+num = Number(num);
+num2 = Number(num2);
 
 let soma = num + num2;
 
-alert('A soma dos números é ' + soma);
+alert(`A soma dos números é ${soma}`);
+
+/* OU 
+*
+* alert(`A soma dos números é ${num + num2}`); 
+*/
