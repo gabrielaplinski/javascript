1b78e41bc5eab3c4ccb1a32b4941969255597fc0
Gabriela Piovesan Plinski <gabriela.dev@outlook.com>
2025-07-09 00:12:44 -0300

    Update index.js

--- aula014/index.js
+++ aula014/index.js
@@ -1,4 +1,14 @@
-let num1 = 1;
-let num2 = 1.4;
+let num1 = 0.7;
+let num2 = 0.1;
 
-console.log(num1.toString() + num2);
+
+num1 = ((num1 * 100) + (num2 * 100)) / 100;
+num1 = ((num1 * 100) + (num2 * 100)) / 100;
+num1 = ((num1 * 100) + (num2 * 100)) / 100;
+
+// num1 = parseFloat(num1.toFixed(2));
+
+console.log(num1);                  
+
+//console.log(num1.toString() + num2);
+console.log(Number.isInteger(num1));
