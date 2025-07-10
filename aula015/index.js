4a424942442531d3a3fe7bd18bff73521a4a2055
Gabriela Piovesan Plinski <gabriela.dev@outlook.com>
2025-07-09 22:44:28 -0300

    Create index.js

---
+++ aula015/index.js
@@ -0,0 +1,9 @@
+let num1 = 9.23123;
+// let num2 = Math.floor(num1);
+// let num2 = Math.ceil(num1);
+// let num2 = Math.round(num1);
+// console.log(Math.max(1, 2, 4, 50, -50, -10, 55, 200));
+let min = 5;
+let max = 10;
+const aleatorio = Math.round(Math.random() * (max - min) + 5); // filtra números aleatórios de 5 a 10
+console.log(aleatorio);
