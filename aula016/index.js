17c8cb03f749e4033dbb06b55c09991cf1519d9c
Gabriela Piovesan Plinski <gabriela.dev@outlook.com>
2025-07-22 22:35:10 -0300

    Create index.js

---
+++ aula016/index.js
@@ -0,0 +1,11 @@
+const alunas = ['Gabriela', 'Aurora', 'Rafael', 1, true, null];
+
+alunas[5] = 'Mojito';
+// alunas[6] = 'Farelo'; = 
+// alunas[alunos.length] = 'Farelo'; =
+alunas.push('Farelo');
+alunas.push('Trevor');
+alunas.push('Brisa');
+console.log(alunas);
+
+//console.log(alunas[5]);
