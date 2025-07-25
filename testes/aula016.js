f52bd70d0f26da00e4af412030622629091aff3a
Gabriela Piovesan Plinski <gabriela.dev@outlook.com>
2025-07-24 22:36:42 -0300

    Create aula016.js

---
+++ testes/aula016.js
@@ -0,0 +1,9 @@
+//OBJETOS
+
+let exemplo = { nome: "Aurora" }; // Aponta para caixa na memória. Ex.: #123 (dentro da caixa contém "{ nome: "Aurora" }")
+
+exemplo.nome = "Gabriela"; // Abre a caixa #123 e troca o seu conteúdo
+
+exemplo = { nome: "Rafael" }; // exemplo passa a apontar para uma nova caixa. A antiga continua existindo.
+
+console.log(exemplo);
