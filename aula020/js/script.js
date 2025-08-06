2370e793b4288f8d29febf321fca07c5451a7233
Gabriela Piovesan Plinski <gabrielaplinski@outlook.com>
2025-08-05 23:12:50 -0300

    att script

---
+++ aula020/js/script.js
@@ -0,0 +1,14 @@
+function Exemplo () {
+    const form = document.querySelector('.form');
+
+    /* Impede a atualização da página onsubmit (ao clicar no enviar):
+    form.onsubmit = function (evento) {
+        evento.preventDefault();
+        alert('Deseja enviar formulário?')
+        console.log('Foi enviado.')
+    };
+    */
+}
+
+Exemplo();
+
