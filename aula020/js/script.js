8e8532a636f102f5915bbf7ef87e62f276f5733f
Gabriela Piovesan Plinski <gabrielaplinski@outlook.com>
2025-08-18 22:18:02 -0300

    ex form

--- aula020/js/script.js
+++ aula020/js/script.js
@@ -1,5 +1,8 @@
 function Exemplo () {
     const form = document.querySelector('.form');
+    const resultado = document.querySelector('.resultado');
+
+    const pessoas = [];
 
     /* Impede a atualização da página onsubmit (ao clicar no enviar):
     form.onsubmit = function (evento) {
@@ -8,6 +11,29 @@ function Exemplo () {
         console.log('Foi enviado.')
     };
     */
+    function recebeEventoForm (evento) {
+        evento.preventDefault();
+
+        const nome = form.querySelector('.nome');
+        const sobrenome = form.querySelector('.sobrenome');
+        const peso = form.querySelector('.peso');
+        const altura = form.querySelector('.altura');
+
+        console.log(nome.value, sobrenome.value, peso.value, altura.value);
+
+        pessoas.push({
+            nome: nome.value,
+            sobrenome: sobrenome.value,
+            peso: peso.value,
+            altura: altura.value,
+        });
+
+        console.log(pessoas);
+
+        resultado.innerHTML += `<p>${nome.value} ${sobrenome.value} ${peso.value} ${altura.value}</p>`
+    };
+
+    form.addEventListener('submit', recebeEventoForm);
 }
 
 Exemplo();
