247a544be72976a5f91525aca9dd7420e8f13018
Gabriela Piovesan Plinski <gabrielaplinski@outlook.com>
2026-01-19 23:52:54 -0300

    Create index.js

---
+++ aula022/index.js
@@ -0,0 +1,19 @@
+/*
+Operadores lógicos
+
+&& -> and
+|| -> or
+! -> not
+*/
+
+/*
+const exemploAnd = true && true && true && true;
+const exemploOr = true || true;
+console.log(exemploOr);
+*/
+
+const usuario = 'Gabriela'
+const senha = '12345'
+
+const loginSuccess = usuario === 'Gabriela' && senha === '12345';
+console.log(loginSuccess);
