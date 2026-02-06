c40c983d2bc69ee0bd5d2350864506838bc9d45b
Gabriela Piovesan Plinski <gabrielaplinski@outlook.com>
2026-02-05 22:20:05 -0300

    add: js

---
+++ aula026/assets/js/main.js
@@ -0,0 +1,51 @@
+// captura evento de submit do form
+const form = document.querySelector('#form')
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const inputPeso = e.target.querySelector('#peso');
+    const inputAltura = e.target.querySelector('#altura');
+
+    const peso = Number(inputPeso.value);
+    const altura = Number(inputAltura.value);
+
+    //se for NaN
+    if (!peso) {
+        setResultado('Peso inválido', false);
+        return;
+    }
+    if (!altura) {
+        setResultado('Altura inválida', false);
+        return;
+    }
+
+    const imc = getImc(peso, altura);
+    console.log('Cheguei');
+})
+
+/*const peso = document.getElementById('#peso');
+const altura = document.getElementById('#altura');
+const imc = peso / (altura * altura);
+
+console.log(imc);*/
+function getImc () {
+    
+}
+
+//criação de parágrafos
+
+function criaPar () {
+    const p = document.createElement('p');
+    return p;
+}
+
+// div #resultado
+
+function setResultado (texto, isValid) {
+    const resultado = document.querySelector('#resultado');
+    resultado.innerHTML = '';
+
+    const p = criaPar();
+    p.innerHTML = texto;
+    resultado.appendChild(p);
+}
